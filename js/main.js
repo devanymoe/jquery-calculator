39dfcf605008c7562996fb452ba6b9700da60c47
Devany Moe <devanymoe@gmail.com>
2016-02-29 22:50:48 -0800

    Fix bugs and lint

--- js/main.js
+++ js/main.js
@@ -1,73 +1,85 @@
 $(document).ready(function() {
+  'use strict';
   var $screen = $('#screen');
   var screenTxt = $screen.text();
   var num1 = null;
   var num2 = null;
+  var numTwoString = '';
   var operator = null;
   var total = null;
+
+  var totaler = function() {
+    num2 = parseInt(numTwoString);
+    if (operator === '+') {
+      total = num1 + num2;
+    }
+    else if (operator === '-') {
+      total = num1 - num2;
+    }
+    else if (operator === '*') {
+      total = num1 * num2;
+    }
+    else if (operator === '/') {
+      total = num1 / num2;
+    }
+    $screen.text(total);
+    screenTxt = total;
+    num1 = total;
+    num2 = null;
+    numTwoString = '';
+  };
+
+  var setOperator = function(sign) {
+    if (numTwoString !== '') {
+      totaler();
+    }
+    num1 = parseInt(screenTxt);
+    $screen.text(screenTxt += sign);
+    operator = sign;
+  };
 
   $('span:not(.operator)').on('click', function() {
     var txt = $(this).text();
     var num = parseInt(txt);
-    if(screenTxt === '0') {
+
+    if (screenTxt === '0') {
       screenTxt = num.toString();
       $screen.text(num);
     }
     else {
       $screen.text(screenTxt += num);
-      if(num1 !== null){
-        num2 += num;
+      if (num1 !== null) {
+        numTwoString += num;
       }
     }
   });
 
   $('.operator').on('click', function() {
-    var txt = $(this).text()
-    if(txt === 'x') {
-      num1 = parseInt(screenTxt);
-      $screen.text(screenTxt += '*');
-      operator = 'multiply';
+    var txt = $(this).text();
+
+    if (txt === 'x') {
+      setOperator('*');
     }
-    else if(txt === '+') {
-      num1 = parseInt(screenTxt);
-      $screen.text(screenTxt += '+');
-      operator = 'add';
+    else if (txt === '+') {
+      setOperator('+');
     }
-    else if(txt === '-') {
-      num1 = parseInt(screenTxt);
-      $screen.text(screenTxt += '-');
-      operator = 'subtract';
+    else if (txt === '-') {
+      setOperator('-');
     }
-    else if(txt === '=') {
-      if (operator === 'add') {
-        total = num1 + num2;
-      }
-      else if (operator === 'subtract') {
-        total = num1 - num2;
-      }
-      else if (operator === 'multiply') {
-        total = num1 * num2;
-      }
-      else if (operator === 'divide') {
-        total = num1 / num2;
-      }
-      $screen.text(total);
-      screenTxt = total;
-      num1 = total;
-      num2 = null;
+    else if (txt === '=') {
+      totaler();
     }
-    else if(txt === 'C') {
+    else if (txt === 'C') {
       screenTxt = '0';
       $screen.text(0);
       num1 = null;
       num2 = null;
       operator = null;
       total = null;
+      numTwoString = '';
     }
     else {
-      $screen.text(screenTxt += '/');
-      num1 = parseInt(screenTxt);
-      operator = 'divide';
+      setOperator('/');
     }
   });
 });
